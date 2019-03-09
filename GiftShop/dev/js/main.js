2fdedcac6f868bb1a91244f2c83374459195da2f
K0nfy <27099419+K0nfy@users.noreply.github.com>
2019-03-09 17:56:40 +0200

    GiftShop.psd: adaptive for a mobiles widths

--- GiftShop/dev/js/main.js
+++ GiftShop/dev/js/main.js
@@ -12,10 +12,15 @@ $(document).ready(function(){
         settings: {
           slidesToShow: 2
         }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1
+        }
       }
     ]
   });
-
 
   $('.featured-products__slider').slick({
     prevArrow: $('.featured-products__slider-arrow--left'),
@@ -30,7 +35,27 @@ $(document).ready(function(){
         settings: {
           slidesToShow: 3
         }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1
+        }
       }
     ]
   });
+});
+
+
+
+// menu button
+$('.menu-button--open')
+  $('.menu-button-container').bind('click', function() {
+    $('.header__navigation').css('marginLeft', '0');
+    $('body').css('overflow', 'hidden');
+});
+
+$('.menu-button--close').bind('click', function() {
+  $('.header__navigation').css('marginLeft', '');
+  $('body').css('overflow', '');
 });
